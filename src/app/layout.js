5908c41f5797a8cf381ee8aa27abd9118ba5e041
xanf-code <darshanaswath@gmail.com>
2025-02-17 14:02:34 -0500

    Docker creation

--- src/app/layout.js
+++ src/app/layout.js
@@ -3,6 +3,7 @@ import "./globals.css";
 import { cn } from "@/lib/utils";
 import { constructMetadata } from "@/lib/metadata";
 import ReactQueryProvider from "@/lib/queryProvider";
+import { Analytics } from "@vercel/analytics/react";
 
 const ibmPlexSans = IBM_Plex_Sans({
   variable: "--font-ibm-plex-sans",
@@ -17,6 +18,7 @@ export default function RootLayout({ children }) {
     <html lang="en" className="dark dark:text-white">
       <body className={cn("antialiased", ibmPlexSans.variable)}>
         <ReactQueryProvider>{children}</ReactQueryProvider>
+        <Analytics />
       </body>
     </html>
   );
