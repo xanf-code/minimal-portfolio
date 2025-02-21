a6d7b7a0743dda59dcb805780a8bdab58998e50e
xanf-code <darshanaswath@gmail.com>
2025-02-20 23:39:14 -0500

    dynamic url

--- src/actions.js
+++ src/actions.js
@@ -1,10 +1,26 @@
 "use server";
+import {
+  GetSecretValueCommand,
+  SecretsManagerClient,
+} from "@aws-sdk/client-secrets-manager";
 
 export const fetchResume = async (collection) => {
+  const client = new SecretsManagerClient({
+    region: process.env.AWS_REGION,
+    credentials: {
+      accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+      secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+    },
+  });
+
+  const command = new GetSecretValueCommand({
+    SecretId: process.env.SECRET_ID,
+  });
+  const response = await client.send(command);
+  const updated_url = JSON.parse(response.SecretString).invoke_url;
+
   try {
-    const response = await fetch(
-      `https://udjysea3jk.execute-api.us-east-1.amazonaws.com/prod/${collection}`
-    );
+    const response = await fetch(`${updated_url}/${collection}`);
     if (!response.ok) {
       throw new Error("Network response was not ok");
     }
--- src/db/init.js
+++
@@ -1,5 +0,0 @@
-import PocketBase from "pocketbase";
-
-const pb = new PocketBase(process.env.POCKET_BASE_URL);
-
-export default pb;
